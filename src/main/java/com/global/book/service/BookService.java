d0bdb551a2d1f834ed19a54efd192845f0479f53
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-02-10 23:31:34 +0200

    Custom Logging

--- src/main/java/com/global/book/service/BookService.java
+++ src/main/java/com/global/book/service/BookService.java
@@ -4,6 +4,8 @@ import com.global.book.Base.BaseService;
 import com.global.book.entity.Auther;
 import com.global.book.entity.Book;
 import com.global.book.repository.BookRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,8 @@ public class BookService extends BaseService<Book, Long> {
 
     @Autowired
     private BookRepo bookRepo;
+
+    Logger log = LoggerFactory.getLogger(BookService.class);
 
     public List<Book> insertAll(List<Book> entities) {
         return bookRepo.saveAll(entities);
